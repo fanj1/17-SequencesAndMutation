ea80a596354db43be7ac72cb3c529314c8d5a85f
fanj1 <fanj@rose-hulman.edu>
2018-04-19 10:18:20 -0400

    Jun Fan

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -251,9 +251,17 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    count = 0
+    letter = s[0]
+    for k in range(1, len(s)):
+        if s[k] == letter:
+            count = count + 1
+        letter = s[k]
+    return count
 
 
 def run_test_is_palindrome():
@@ -324,7 +332,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # done: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -333,6 +341,12 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ####################################################################
     # ------------------------------------------------------------------
+
+    ans = True
+    for k in range(len(s) // 2):
+        if s[k] != s[len(s) - k - 1]:
+            ans = False
+    return ans
 
 
 # ----------------------------------------------------------------------
@@ -391,9 +405,15 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    ans = 0
+    for k in range(len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            ans = ans + 1
+    return ans
 
 
 # ----------------------------------------------------------------------
